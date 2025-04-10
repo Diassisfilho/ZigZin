93272067578f3e0220fa4abe70009b0d9ae1787c
deAssis Filho <barroso.assis.francisco@gmail.com>
2025-04-09 21:09:32 -0300

    Updated automato of lexer analyzer to support more lexemes and updated zig test code

--- tests/lexer_input_test.zig
+++ tests/lexer_input_test.zig
@@ -1,4 +1,10 @@
 pub fn main() void {
-    const pi = 3.1415;
+    var pi = 3.1415;
+    while (true) {
+        pi = pi + 2;
+        if (pi > 50) {
+            break;
+        }
+    }
     _ = pi + 10 + 2;
 }
